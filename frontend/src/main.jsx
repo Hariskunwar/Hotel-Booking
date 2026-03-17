8c2d3e71334df992611328d03d0439392c7e490f
Harish Kunwar <harishkunwar755@gmail.com>
2026-03-17 10:41:22 +0545

    feat: implement homepage layout and components

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
+
+const VITE_CLERK_PUBLISHABLE_KEY=import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+     <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </BrowserRouter>,
+)
