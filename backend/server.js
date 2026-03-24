110576813936bd06a892506734e99106c0c50c4d
Harish Kunwar <harishkunwar755@gmail.com>
2026-03-24 15:01:19 +0545

    feat: integrate Clerk for user authentication

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDb from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 const app=express();
 app.use(cors());
 connectDb();
+//middleware
+app.use(express.json());
+app.use(clerkMiddleware())
+
+app.use('/api/clerk',clerkWebhooks)
 
 
 const PORT=process.env.PORT||4000;
